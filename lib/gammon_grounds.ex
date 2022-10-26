9815a235ea5df37797daafbff71459b38b8219cb
Arie Milner <arie.milner@hey.com>
2022-10-26 11:26:14 +0200

    mix new gammon_grounds

---
+++ lib/gammon_grounds.ex
@@ -0,0 +1,18 @@
+defmodule GammonGrounds do
+  @moduledoc """
+  Documentation for `GammonGrounds`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GammonGrounds.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gammon_grounds_test.exs
@@ -0,0 +1,8 @@
+defmodule GammonGroundsTest do
+  use ExUnit.Case
+  doctest GammonGrounds
+
+  test "greets the world" do
+    assert GammonGrounds.hello() == :world
+  end
+end
