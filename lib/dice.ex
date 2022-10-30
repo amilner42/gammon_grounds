3427e08221199a69e4fa4a49040ebb13e61d9968
Arie Milner <arie.milner@hey.com>
2022-10-30 10:47:48 +0100

    Misc minor fn cleanup

--- lib/dice.ex
+++ lib/dice.ex
@@ -3,7 +3,6 @@ defmodule Dice do
     [Enum.random(1..6), Enum.random(1..6)]
   end
 
-  def double?([die_1, die_2]) do
-    die_1 == die_2
-  end
+  def double?([die_1, die_1]), do: true
+  def double?(_dice_roll), do: false
 end
