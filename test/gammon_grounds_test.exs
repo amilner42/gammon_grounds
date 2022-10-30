171e1cdc2f2804a76837e5aca04429e593ce9b29
Arie Milner <arie.milner@hey.com>
2022-10-30 17:14:23 +0100

    Formatting, test for fn in describe

--- test/gammon_grounds_test.exs
+++ test/gammon_grounds_test.exs
@@ -3,47 +3,47 @@ defmodule GammonGroundsTest do
   use ExUnit.Case
   doctest GammonGrounds
 
-  test "A standard opening 6-5 play is legal" do
-    board = new_board()
-    dice = [6, 5]
-    standard_opening_6_5_play = [new_checker_move(24, 18), new_checker_move(18, 13)]
+  describe "#legal_move?" do
+    test "A standard opening 6-5 play is legal" do
+      board = new_board()
+      dice = [6, 5]
+      standard_opening_6_5_play = [new_checker_move(24, 18), new_checker_move(18, 13)]
 
-    assert legal_turn_move?(board, standard_opening_6_5_play, dice) == true
-  end
+      assert legal_turn_move?(board, standard_opening_6_5_play, dice) == true
+    end
 
-  test "It is illegal to use one die when you can use both" do
-    board = new_board()
-    dice = [6, 5]
-    illegal_single_dice_move = [new_checker_move(24, 18)]
+    test "It is illegal to use one die when you can use both" do
+      board = new_board()
+      dice = [6, 5]
+      illegal_single_dice_move = [new_checker_move(24, 18)]
 
-    assert legal_turn_move?(board, illegal_single_dice_move, dice) == false
-  end
+      assert legal_turn_move?(board, illegal_single_dice_move, dice) == false
+    end
 
-  test "A legal double die turn move is legal" do
-    # This is contrived as you can never get a double on the first roll, but, it's all the same for testing.
-    board = new_board()
-    dice = [6, 6]
+    test "A legal double die turn move is legal" do
+      # This is contrived as you can never get a double on the first roll, but, it's all the same for testing.
+      board = new_board()
+      dice = [6, 6]
 
-    legal_double_move = [
-      new_checker_move(24, 18),
-      new_checker_move(24, 18),
-      new_checker_move(13, 7),
-      new_checker_move(13, 7)
-    ]
+      legal_double_move = [
+        new_checker_move(24, 18),
+        new_checker_move(24, 18),
+        new_checker_move(13, 7),
+        new_checker_move(13, 7)
+      ]
 
-    assert legal_turn_move?(board, legal_double_move, dice) == true
-  end
+      assert legal_turn_move?(board, legal_double_move, dice) == true
+    end
 
-  test "Empty turn is legal when there is no move" do
-    board = new_board({:manual_position, player_1(), %{6 => 2}, %{24 => 2}})
-    dice = [5, 5]
+    test "Empty turn is legal when there is no move" do
+      board = new_board({:manual_position, player_1(), %{6 => 2}, %{24 => 2}})
+      dice = [5, 5]
 
-    legal_no_op_turn_move = []
+      legal_no_op_turn_move = []
 
-    assert legal_turn_move?(board, legal_no_op_turn_move, dice) == true
-  end
+      assert legal_turn_move?(board, legal_no_op_turn_move, dice) == true
+    end
 
-  describe "bearing off" do
     test "You can bear off checkers when all checkers are in the homeboard" do
       board = new_board({:manual_position, player_1(), %{6 => 2, 5 => 1}, %{24 => 2}})
       dice = [6, 5]
